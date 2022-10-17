c1d700e2420c3f226c2725fc25404a3284ab759e
James Pritchard <james.edward.pritchard@gmail.com>
2022-10-17 04:50:10 -0400

    main page setup

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -2,13 +2,11 @@ import React from 'react'
 
 
 // Component Imports
-import Navbar from '../../components/Navbar/Navbar'
 import LoginForm from '../../components/Forms/LoginForm'
 
 function Login() {
   return (
     <>
-        <Navbar />
         <LoginForm/>
     </>
   )
--- client/src/pages/Signup/Signup.jsx
+++ client/src/pages/Signup/Signup.jsx
@@ -2,13 +2,11 @@ import React from 'react'
 
 
 // Component Imports
-import Navbar from '../../components/Navbar/Navbar'
 import SignUpForm from '../../components/Forms/SignUpForm'
 
 function SignUp() {
   return (
     <div>
-        <Navbar />
         <SignUpForm/>
     </div>
   )
