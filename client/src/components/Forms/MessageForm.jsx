f1fd229c6a4a591bced55947b73716585edcdf2f
James Pritchard <james.edward.pritchard@gmail.com>
2022-10-22 12:39:36 -0400

    additional message form work

--- client/src/components/Forms/MessageForm.jsx
+++ client/src/components/Forms/MessageForm.jsx
@@ -11,20 +11,20 @@ function MessageForm() {
     <div>
         <form className='form'>
                     <div className='form-field message'>
-                    <div className='form-field message '>
-                        <label htmlFor="community">Community</label>
-                        <select name= 'community' type='community' id='community'
+                    <div className='form-field message'>
+                        <label htmlFor="recipients">Send Message To:</label>
+                        <select name= 'recipients' type='recipients' id='recipients'
                             onChange={handleChange}>
-                                <option value="null">Select Your Community</option>
+                                <option value="null">Select Recipients</option>
                                 {/* {communityNamesArray.map((community)=>{
                                     return <option value={community._id} key={community._id}>{community.communityname}</option>
                                 })} */}
                             </select>
                     </div>
                     </div>
-                    <div className='form-field message'>
-                        <label htmlFor="password">Message</label>
-                        <textarea name='password' type='password' id='password' autoComplete="current-password"
+                    <div className='form-field message messagetextarea'>
+                        <label htmlFor="message">Message</label>
+                        <textarea name='message' type='message' id='message'
                             onChange={handleChange}></textarea>
                     </div>
                     <button className='form-field message form-field-button ' type='button' onClick={handleFormSubmit}>Send Message </button>
