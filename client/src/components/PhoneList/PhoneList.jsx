85937cc2556af289ca0c830d4e1abbe753920083
James Pritchard <james.edward.pritchard@gmail.com>
2022-11-07 04:10:44 -0500

    phonelist work

--- client/src/components/PhoneList/PhoneList.jsx
+++ client/src/components/PhoneList/PhoneList.jsx
@@ -7,13 +7,13 @@ function PhoneList() {
   const [viewContacts, setViewContacts] = useState(false);
 
    //get contact list titles
-   let contactlistTitleArray;
+   let contactlistArray;
 
    const { loading, error, data } = useQuery(GET_CONTACTLISTS);
    if (loading) return 'Loading...';
    if (error) return `Error! ${error.message}`;
    if(!loading && !error){
-   contactlistTitleArray = data.contactList.contactlists
+   contactlistArray = data.contactList.contactlists
    }
   const openContacts = () =>{
     setViewContacts(true)
@@ -22,7 +22,16 @@ function PhoneList() {
     <div>
       {viewContacts ?
       <div className="contact-list">
-
+        {contactlistArray.map((contactlists) =>{
+          return contactlists.contacts.map((contact)=>{
+            return <div>
+              <p>{contact.firstname}</p>
+              <p>{contact.lastname}</p>
+              <p>{contact.phonenumber}</p>
+            </div>
+          });
+        }
+        )}
       </div>
       :
       <button className="phone-list-button" onClick={openContacts}>Contacts</button>
